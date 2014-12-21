1fe197a42fde22c6e5987039d71fb3db0d355d63
XuKemPT <ptxuxu@gmail.com>
2014-12-21 23:23:04 +0700

    update DATABASE

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141211071233) do
+ActiveRecord::Schema.define(version: 20141221161204) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -48,8 +48,22 @@ ActiveRecord::Schema.define(version: 20141211071233) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
+  create_table "assets", force: true do |t|
+    t.string   "storage_uid"
+    t.string   "storage_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "storage_width"
+    t.integer  "storage_height"
+    t.float    "storage_aspect_ratio", limit: 24
+    t.integer  "storage_depth"
+    t.string   "storage_format"
+    t.string   "storage_mime_type"
+    t.string   "storage_size"
+  end
+
   create_table "categories", force: true do |t|
-    t.string   "name",       null: false
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -69,8 +83,10 @@ ActiveRecord::Schema.define(version: 20141211071233) do
     t.integer  "system_id"
   end
 
+  add_index "elements", ["name"], name: "name", unique: true, using: :btree
+
   create_table "images", force: true do |t|
-    t.text     "image_path"
+    t.string   "image_path"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "attachment"
@@ -91,7 +107,7 @@ ActiveRecord::Schema.define(version: 20141211071233) do
     t.string   "name"
     t.text     "description"
     t.integer  "status_id"
-    t.datetime "lauch_date"
+    t.datetime "launch_date"
     t.datetime "active_date"
     t.datetime "finished_date"
     t.datetime "created_at"
